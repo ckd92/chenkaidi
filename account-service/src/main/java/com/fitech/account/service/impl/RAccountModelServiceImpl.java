edcc747fe9f6c73604278800997cf85e9b77909c
124264956 <124264956@qq.com>
2018-11-27 11:18:54 +0800

    修改

--- account-service/src/main/java/com/fitech/account/service/impl/RAccountModelServiceImpl.java
+++ account-service/src/main/java/com/fitech/account/service/impl/RAccountModelServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fitech.account.repository.AccountProcessRepository;
+import com.fitech.domain.account.Account;
 import com.fitech.domain.account.AccountProcess;
 import com.fitech.framework.core.trace.ServiceTrace;
 import com.fitech.report.external.RAccountModelService;
@@ -17,6 +18,16 @@ public class RAccountModelServiceImpl implements RAccountModelService {
 	@Override
 	public void saveAccountProcess(AccountProcess accountProcess) {
 		accountProcessRepository.save(accountProcess);
+	}
+
+	@Override
+	public AccountProcess findAccountProcess(Long processId) {
+		return accountProcessRepository.findOne(processId);
+	}
+
+	@Override
+	public AccountProcess findAccountProcess(Account account) {
+		return accountProcessRepository.findByAccount(account);
 	}
 
 }
