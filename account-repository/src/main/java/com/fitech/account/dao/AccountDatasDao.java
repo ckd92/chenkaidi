5e21cf814f735976d1ac9d77cd597bc923dd9915
yangfan <Yangfan@fitech.com.cn>
2018-12-29 08:49:44 +0800

    紫金补录装载功能修改

--- account-repository/src/main/java/com/fitech/account/dao/AccountDatasDao.java
+++ account-repository/src/main/java/com/fitech/account/dao/AccountDatasDao.java
@@ -1,7 +1,10 @@
 package com.fitech.account.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import com.fitech.domain.ledger.LedgerReportTemplate;
+import com.fitech.framework.lang.result.GenericResult;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import com.fitech.domain.account.Account;
@@ -10,7 +13,7 @@ import com.fitech.domain.account.AccountTemplate;
 public interface AccountDatasDao {
 	
 	/**
-	 * 
+	 * 载入数据，删除原有数据，新增添加的数据。
 	 * @param accountId
 	 * @param accountTemplate
 	 * @param sheet
@@ -18,4 +21,15 @@ public interface AccountDatasDao {
 	 * @return
 	 */
 	public List<String> loadDataByTemplate(Long accountId, AccountTemplate accountTemplate, Sheet sheet,Account account);
+
+	/**
+	 * 载入数据，覆盖主键重复，新增不重复数据
+	 * @param accountId
+	 * @param accountTemplate
+	 * @param sheet
+	 * @param account
+	 * @return
+	 */
+	public Map<String,Object> loadDataByExcel(Long accountId, AccountTemplate accountTemplate, Sheet sheet, Account account);
+
 }
